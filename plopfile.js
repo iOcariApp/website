f39abba698374f06bbd23b7592436d37151e008c
Enzo Ferey <hello@enzoferey.com>
2018-07-18 19:42:11 +0200

    create-react-app base + plop setup.

---
+++ plopfile.js
@@ -0,0 +1,42 @@
+module.exports = function(plop) {
+	// controller generator
+	plop.setGenerator("controller", {
+		description: "React component generator",
+		prompts: [
+			{
+				type: "input",
+				name: "componentName",
+				message: "component name please"
+			},
+			{
+				type: "input",
+				name: "path",
+				message: "absolute path please"
+			}
+		],
+		actions: [
+			{
+				type: "add",
+				path: "{{path}}/{{componentName}}/{{componentName}}.js",
+				templateFile: "templates/BasicComponent/BasicComponent.js.hbs"
+			},
+			{
+				type: "add",
+				path:
+					"{{path}}/{{componentName}}/{{dashCase componentName}}.scss",
+				templateFile: "templates/BasicComponent/BasicComponent.scss.hbs"
+			},
+			{
+				type: "add",
+				path: "{{path}}/{{componentName}}/{{componentName}}.test.js",
+				templateFile: "templates/BasicComponent/BasicComponent.test.js.hbs"
+			},
+			{
+				type: "add",
+				path:
+					"{{path}}/{{componentName}}/index.js",
+				templateFile: "templates/export.js.hbs"
+			}
+		]
+	});
+};
