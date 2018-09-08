3a09f1e9fed2e97ae0dc72bc6912c2271272720f
Enzo Ferey <hello@enzoferey.com>
2018-09-08 16:02:56 +0200

    Split logic of country voting. Map basic layout.

--- src/components/CountriesVote/CountriesVote.js
+++ src/components/CountriesVote/CountriesVote.js
@@ -1,17 +1,121 @@
 import React from "react";
+import PropTypes from "prop-types";
 import style from "./countries-vote.scss";
 
 import Media from "react-media";
 
 import variables from "components/variables.scss";
 
+import InputWithButton from "components/InputWithButton";
 import CountriesCards from "components/CountriesCards";
 import Map from "components/Map";
 
-const CountriesVote = () => (
-  <Media query={{ maxWidth: variables.mdAnchor }}>
-    {matches => (matches ? <CountriesCards /> : <Map />)}
-  </Media>
-);
+const options = [
+  { value: "España", label: "España" },
+  { value: "Chile", label: "Chile" },
+  { value: "Cuba", label: "Cuba" },
+  {
+    value: "Argentina",
+    label: "Argentina",
+  },
+  { value: "México", label: "México" },
+];
+
+class CountriesVote extends React.Component {
+  state = {
+    voted: false,
+    countries: this.props.countries,
+    search: "",
+  };
+
+  onChange = search => {
+    this.setState({ search });
+  };
+
+  onVote = votedCountry => {
+    const { voted, countries } = this.state;
+
+    if (votedCountry === "") return;
+
+    const copy = countries;
+    const countryIndex = copy.findIndex(
+      country => country.name.toLowerCase() === votedCountry.toLowerCase()
+    );
+
+    if (countryIndex >= 0) {
+      const toAdd = voted ? -1 : 1;
+      copy[countryIndex].votes = copy[countryIndex].votes + toAdd;
+
+      this.setState(state => ({ voted: !state.voted, countries: copy }));
+    }
+  };
+
+  render() {
+    const { voted } = this.state;
+
+    return (
+      <Media query={{ maxWidth: variables.mdAnchor }}>
+        {matches => (
+          <div className={style.main}>
+            <div className={style.maxWidth}>
+              <h1 className={style.sectionTitle}>¿Dónde jugamos ahora?</h1>
+              <p className={style.explanation}>
+                {matches
+                  ? "Si quieres que estemos disponibles en tu país, dale +1 búscandolo aquí:"
+                  : "Si quieres que estemos disponibles en tu país, dale +1 en el mapa o búscalo aquí:"}
+              </p>
+              <div className={style.inputWithButton}>
+                <InputWithButton
+                  placeholder="País"
+                  inputClass={style.input}
+                  buttonText={voted ? "- 1" : "+ 1"}
+                  buttonClass={style.inputButton}
+                  onClick={this.onVote}
+                  onChange={this.onChange}
+                  options={options}
+                />
+              </div>
+              {matches ? <CountriesCards {...this.state} /> : <Map />}
+            </div>
+          </div>
+        )}
+      </Media>
+    );
+  }
+}
+
+CountriesVote.propTypes = {
+  countries: PropTypes.array,
+};
+
+CountriesVote.defaultProps = {
+  countries: [
+    {
+      name: "España",
+      votes: 10,
+      exists: true,
+    },
+    {
+      name: "México",
+      votes: 3,
+      exists: false,
+    },
+    {
+      name: "Chile",
+      votes: 20,
+      exists: false,
+    },
+    {
+      name: "Argentina",
+      votes: 7,
+      exists: false,
+    },
+    {
+      name: "Cuba",
+      votes: 13,
+      exists: false,
+    },
+  ],
+};
 
 export default CountriesVote;
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,8 +2,73 @@ import React from "react";
 import PropTypes from "prop-types";
 import style from "./map.scss";
 
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography,
+} from "react-simple-maps";
+
+import atlas from "./atlas.json";
+
+const wrapperStyles = {
+  width: "100%",
+  maxWidth: 980,
+  margin: "0 auto",
+};
+
+const include = ["Antarctica"];
+
 const Map = () => (
-  <p>Map</p>
-)
+  <div style={wrapperStyles}>
+    <ComposableMap
+      projectionConfig={{ scale: 300 }}
+      width={1400}
+      height={800}
+      style={{
+        width: "100%",
+        height: "auto",
+      }}
+    >
+      <ZoomableGroup center={[0, 20]} disablePanning>
+        <Geographies geography={atlas}>
+          {(geographies, projection) =>
+            geographies.map(
+              (geography, i) =>
+                console.log(geography) ||
+                (include.indexOf(geography.properties.CONTINENT) === -1 && (
+                  <Geography
+                    key={i}
+                    geography={geography}
+                    projection={projection}
+                    style={{
+                      default: {
+                        fill: "#028C9B",
+                        stroke: "#FFFFFF",
+                        strokeWidth: 0.5,
+                        outline: "none",
+                      },
+                      hover: {
+                        fill: "#CFD8DC",
+                        stroke: "#607D8B",
+                        strokeWidth: 0.75,
+                        outline: "none",
+                      },
+                      pressed: {
+                        fill: "#FF5722",
+                        stroke: "#607D8B",
+                        strokeWidth: 0.75,
+                        outline: "none",
+                      },
+                    }}
+                  />
+                ))
+            )
+          }
+        </Geographies>
+      </ZoomableGroup>
+    </ComposableMap>
+  </div>
+);
 
 export default Map;
